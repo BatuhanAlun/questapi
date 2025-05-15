189a460f384d9e0d27d57046fa8f259935f92cd1
Batuhan Alun <22220030071@mersin.edu.tr>
2025-05-15 03:07:42 +0300

    login jwt auth

--- cmd/questapi/main.go
+++ cmd/questapi/main.go
@@ -7,5 +7,7 @@ import (
 
 func main() {
 	http.HandleFunc("/register", handlers.RegisterHandler)
+	http.HandleFunc("/login", handlers.LoginHandler)
+	http.HandleFunc("/quest", handlers.Quest)
 	http.ListenAndServe(":8080", nil)
 }
